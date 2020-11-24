e88a48d5d3f89c1d52ee02b8f528596a5177c84e
Shaik_Z <kz981786@gmail.com>
2020-11-25 01:41:20 +0530

    commit11

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends Component {
                 </Switch>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
-                <LocationDisplay pathname{pathname}/>
+                <LocationDisplay pathname={pathname}/>
             </div>
         )
     }
